d935188ec2f2bc02e00a5a90a112282f5e6e42ec
Parth Darji <parthdarji2961@gmail.com>
2024-12-19 13:13:02 +0530

    first lesson completed

--- warcardgame/ContentView.swift
+++ warcardgame/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color(.black).ignoresSafeArea()
+            VStack {
+                Image("niagara_falls").resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                    .padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
+                Text("Niagara Falls").font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/).fontWeight(.black).foregroundColor(Color.white).textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+            }
         }
-        .padding()
+        
+        
     }
 }
 
