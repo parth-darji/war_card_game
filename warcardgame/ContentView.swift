0f936064aa32822f7979339443220cdf9f782a74
Parth Darji <parthdarji2961@gmail.com>
2024-12-19 16:08:40 +0530

    Created a travel card by using swiftui

--- warcardgame/ContentView.swift
+++ warcardgame/ContentView.swift
@@ -10,14 +10,48 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            Color(.black).ignoresSafeArea()
-            VStack {
+            Color(.systemMint).ignoresSafeArea()
+            VStack(alignment: .leading, spacing: 20.0) {
                 Image("niagara_falls").resizable()
                     .aspectRatio(contentMode: .fit)
                     .cornerRadius(15)
-                    .padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
-                Text("Niagara Falls").font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/).fontWeight(.black).foregroundColor(Color.white).textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                
+                
+                HStack{
+                    Text("Niagara Falls").font(.largeTitle).fontWeight(.bold).foregroundColor(Color.black)
+                    
+                    Spacer()
+                    
+                    
+                    VStack{
+                        HStack{
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                        }
+                        Text("(Reviews 561)")
+                    }.foregroundColor(.orange).font(.caption)
+                }
+                
+                
+                
+                Text("Come visit the falls for an experience of a lifetime.").font(/*@START_MENU_TOKEN@*/.body/*@END_MENU_TOKEN@*/).fontWeight(.bold).foregroundColor(Color.black)
+                
+                HStack{
+                    Spacer()
+                    Image(systemName: "fork.knife")
+                    Image(systemName: "binoculars.fill")
+                }
+                .foregroundColor(.gray).font(.caption)
             }
+            .padding()
+            .background(Rectangle()
+            .foregroundColor(.white))
+            .cornerRadius(15)
+            .shadow(radius: 15)
+            .padding()
         }
         
         
