bc8b419b175893b362d8fb99a9680658f33e094c
Parth Darji <parthdarji2961@gmail.com>
2024-12-19 11:29:33 +0530

    Initial Commit

---
+++ warcardgame/warcardgameApp.swift
@@ -0,0 +1,17 @@
+//
+//  warcardgameApp.swift
+//  warcardgame
+//
+//  Created by Parth Darji on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct warcardgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ warcardgameTests/warcardgameTests.swift
@@ -0,0 +1,17 @@
+//
+//  warcardgameTests.swift
+//  warcardgameTests
+//
+//  Created by Parth Darji on 12/19/24.
+//
+
+import Testing
+@testable import warcardgame
+
+struct warcardgameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
