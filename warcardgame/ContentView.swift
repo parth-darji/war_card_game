c94083857ffecc67cf1255555becdb3ef1bf09fd
Parth Darji <parthdarji2961@gmail.com>
2024-12-26 13:29:51 +0530

    Lesson 7 completed - shown variable in View

--- warcardgame/ContentView.swift
+++ warcardgame/ContentView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    var playerCard = "card7"
+    var playerScore = 0
+    
+    var cpuCard = "card13"
+    var cpuScore = 0
+    
     var body: some View {
         ZStack{
             
@@ -21,17 +27,19 @@ struct ContentView: View {
                 Spacer()
                 HStack {
                     Spacer()
-                    Image("card2")
+                    Image(playerCard)
                     Spacer()
-                    Image("card3")
+                    Image(cpuCard)
                     Spacer()
                 }
                 Spacer()
-                Button("Deal") {
-                    deal()
-                }.foregroundColor(.white)
                 
-//                Image("button")
+                Button{
+                    deal()
+                } label:  {
+                    Image("button")
+                }
+                
                 Spacer()
                 HStack {
                     Spacer()
@@ -39,18 +47,18 @@ struct ContentView: View {
                         Text("Player")
                             .font(.headline)
                             .padding(.bottom, 2.0)
-                           
-                        Text("0")
+                        
+                        Text(String(playerScore))
                             .font(.largeTitle)
-                          
+                        
                     }
                     Spacer()
                     VStack  {
                         Text("CPU")
                             .font(.headline)
                             .padding(.bottom, 2.0)
-     
-                        Text("0")
+                        
+                        Text(String(cpuScore))
                             .font(.largeTitle)
                     }
                     Spacer()
